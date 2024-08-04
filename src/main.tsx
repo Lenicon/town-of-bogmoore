854533e4514617d6bee5ff32623eeb9051e2a79b
Len.icon <lenthesmiledev@gmail.com>
2024-08-05 05:34:23 +0800

    removed large css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
-import './assets/css/twemoji-amazing.css'
+// import 'https://cdn.jsdelivr.net/gh/SebastianAigner/twemoji-amazing/twemoji-amazing.css'
 import './assets/css/recolored-twemoji.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
